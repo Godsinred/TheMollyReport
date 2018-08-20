51a53b5611ecdb3df5882c97a7b93c41373ce5a6
Jonathan_Ishii <jtishii@yahoo.com>
2018-08-20 12:27:11 -0700

    Update

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from fbchat import log, Client
+from fbmessenger import quick_replies
+from fbchat.models import *
 # Subclass fbchat.Client and override required methods
 class EchoBot(Client):
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
@@ -9,7 +11,17 @@ class EchoBot(Client):
             log.info("{} from {} in {}".format(message_object, thread_id, thread_type. name))
             # If you're not the author, echo
             if author_id != self.uid:
-                self.send(message_object, thread_id=thread_id, thread_type=thread_type)
+                quick_reply_1 = quick_replies.QuickReply(title='Location', content_type='location')
+                quick_replies_set = quick_replies.QuickReplies(quick_replies=[
+                    quick_reply_1
+                ])
+                text = {'text': 'Share your location'}
+                text['quick_replies'] = quick_replies_set.to_dict()
+                quick_replies = [
+                    QuickReply(title="Action", payload="PICK_ACTION"),
+                    QuickReply(title="Comedy", payload="PICK_COMEDY")
+                ]
+                self.send(Message(text=text), thread_id=thread_id, thread_type=thread_type)
 
-client = EchoBot("fortnite.killfeed@gmail.com", "fortnite")
+client = EchoBot("TheOfficialMollyReport@gmail.com", "themolly")
 client.listen()
